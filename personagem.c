22d8513dba9169da3f8f74e6b4f158348969f8bf
brunoferreira94 <brunoferreira94@users.noreply.github.com>
2018-06-09 10:07:04 -0300

    Animação personagem

--- personagem.c
+++ personagem.c
@@ -1,10 +1,19 @@
 #include <GL/glut.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
 GLUquadricObj *obj;
 
-float angX = 0;
-float angY = 0;
+float angX = 45;
+float angY = 45;
 float raioCorpo = 0.7, raioMembros=0.2, raioBastao=0.1;
+float trx = 0;
+float try = 0;
+float ombro = 0;
+float antebraco = -90;
+float perna = 90;
+int flag = 0;
 
 void init(){
      glClearColor(0.0,0.0,0.0,0.0);
@@ -24,11 +33,19 @@ void display()
 
     glPopMatrix();
     glPushMatrix();
+    	glTranslatef(trx,try,0);
         glRotatef(angX,1,0,0);
         glRotatef(angY,0,1,0);
         glPushMatrix(); // rosto
             glTranslatef(0,4.1,0);
             glutSolidSphere(1.2,100,100);
+			glPushMatrix(); //olhos
+				glColor3f(0,0,1);
+				glTranslatef(0.4,0.4,0.8);
+				glutSolidSphere(0.3,50,50);
+				glTranslatef(-0.8,0,0);
+				glutSolidSphere(0.3,50,50);
+			glPopMatrix();
         glPopMatrix();
         glPushMatrix(); // corpo
             glColor3f(1.0, 0, 0);
@@ -40,129 +57,140 @@ void display()
 
         glPushMatrix(); // ombro direito
             glTranslatef(.78,2.5,0);
+            glRotatef(ombro,1,0,0);
             glutSolidSphere(0.2,100,100);
-        glPopMatrix();
-        glPushMatrix(); // braço direito
 
-            glColor3f(1.0, 0.85, 0.75);
-            glRotatef(90,1,0,0);
-            glTranslatef(0.75,0,-2.4);
-            gluDisk(obj,0,raioMembros,100,100);
-            gluCylinder(obj, raioMembros, raioMembros, 1.5, 100, 100);
+            glPushMatrix(); // braço direito
 
-            glPushMatrix(); //antebraço direito
-                glTranslatef(0,0,1.6);
-                glRotatef(-90,1,0,0);
-                glutSolidSphere(0.2,100,100); ///cotovelo
+                glColor3f(1.0, 0.85, 0.75);
+                glRotatef(90,2,0,0);
+
                 gluDisk(obj,0,raioMembros,100,100);
-                gluCylinder(obj, raioMembros, raioMembros, 1, 100, 100);
+                gluCylinder(obj, raioMembros, raioMembros, 1.5, 100, 100);
 
-                glPushMatrix(); // mao direita
-                    glColor3f(0,0,1);
-                    glTranslatef(0,0,1);
-                    glutSolidSphere(0.3,100,100);
+                glPushMatrix(); //antebraço direito
+                    glTranslatef(0,0,1.6);
+                    glRotatef(antebraco,1,0,0);
+                    glutSolidSphere(0.2,100,100); ///cotovelo
+                    gluDisk(obj,0,raioMembros,100,100);
+                    gluCylinder(obj, raioMembros, raioMembros, 1, 100, 100);
 
-                    glColor3f(1,1,1);
-                    glRotatef(90,1,0,0);
-                    glTranslatef(0,0,-2.5);
-                    gluDisk(obj,0,raioBastao,100,100);
-                    gluCylinder(obj, raioBastao, raioBastao, 8, 100, 100);
+                    glPushMatrix(); // mao direita
+                        glColor3f(0,0,1);
+                        glTranslatef(0,0,1);
+                        glutSolidSphere(0.3,100,100);
+
+                        glColor3f(0.5,0.5,0.5);
+                        glRotatef(90,1,0,0);
+                        glTranslatef(0,0,-2.5);
+                        gluDisk(obj,0,raioBastao,100,100);
+                        gluCylinder(obj, raioBastao, raioBastao, 8, 100, 100);
+                    glPopMatrix();
                 glPopMatrix();
             glPopMatrix();
         glPopMatrix();
-
         glPushMatrix(); // ombro esquerdo
             glColor3f(1,0,0);
             glTranslatef(-.78,2.5,0);
+            glRotatef(ombro,1,0,0);
             glutSolidSphere(0.2,100,100);
-        glPopMatrix();
-        glPushMatrix(); // braço esquerdo
 
-            glColor3f(1.0, 0.85, 0.75);
-            glRotatef(90,1,0,0);
-            glTranslatef(-0.75,0,-2.4);
-            gluDisk(obj,0,raioMembros,100,100);
-            gluCylinder(obj, raioMembros, raioMembros, 1.5, 100, 100);
+            glPushMatrix(); // braço esquerdo
 
-            glPushMatrix(); //antebraço esquerdo
-                glTranslatef(0,0,1.6);
-                glRotatef(-90,1,0,0);
-                glutSolidSphere(0.2,100,100); ///cotovelo
+                glColor3f(1.0, 0.85, 0.75);
+                glRotatef(90,1,0,0);
                 gluDisk(obj,0,raioMembros,100,100);
-                gluCylinder(obj, raioMembros, raioMembros, 1, 100, 100);
+                gluCylinder(obj, raioMembros, raioMembros, 1.5, 100, 100);
 
-                glPushMatrix(); // mao esquerda
-                    glColor3f(0,0,1);
-                    glTranslatef(0,0,1);
-                    glutSolidSphere(0.3,100,100);
+                glPushMatrix(); //antebraço esquerdo
+                    glTranslatef(0,0,1.6);
+                    glRotatef(antebraco,1,0,0);
+                    glutSolidSphere(0.2,100,100); ///cotovelo
+                    gluDisk(obj,0,raioMembros,100,100);
+                    gluCylinder(obj, raioMembros, raioMembros, 1, 100, 100);
 
-                    glColor3f(1,1,1);
-                    glRotatef(90,1,0,0);
-                    glTranslatef(0,0,-2.5);
-                    gluDisk(obj,0,raioBastao,100,100);
-                    gluCylinder(obj, raioBastao, raioBastao, 8, 100, 100);
+                    glPushMatrix(); // mao esquerda
+                        glColor3f(0,0,1);
+                        glTranslatef(0,0,1);
+                        glutSolidSphere(0.3,100,100);
+
+                        glColor3f(0.5,0.5,0.5);
+                        glRotatef(90,1,0,0);
+                        glTranslatef(0,0,-2.5);
+                        gluDisk(obj,0,raioBastao,100,100);
+                        gluCylinder(obj, raioBastao, raioBastao, 8, 100, 100);
+                    glPopMatrix();
                 glPopMatrix();
             glPopMatrix();
         glPopMatrix();
 
-        glPushMatrix(); //perna direita
-            glColor3f(1.0, 0.85, 0.75);
-            glRotatef(90,1,0,0);
-            glTranslatef(0.4,0,1.9);
-            gluDisk(obj,0,raioMembros,100,100);
-            gluCylinder(obj, raioMembros, raioMembros, 1.5, 100, 100);
+        glPushMatrix();
+            glColor3f(1,1,1);
+            glTranslatef(0.4,-1.9,0);
+            glutSolidSphere(0.22,100,100); ///bumbum tam
+            glPushMatrix(); //perna direita
+                glColor3f(1.0, 0.85, 0.75);
+                glRotatef(perna,1,0,0);
+                gluDisk(obj,0,raioMembros,100,100);
+                gluCylinder(obj, raioMembros, raioMembros, 1.5, 100, 100);
 
-            glTranslatef(0,0,1.5);
-            glutSolidSphere(0.22,100,100); ///joelho
+                glPushMatrix();
+                    glTranslatef(0,0,1.5);
+                    glutSolidSphere(0.22,100,100); ///joelho
 
-            gluDisk(obj,0,raioMembros,100,100);
-            gluCylinder(obj, raioMembros, raioMembros, 1.4, 100, 100);
+                    gluDisk(obj,0,raioMembros,100,100);
+                    gluCylinder(obj, raioMembros, raioMembros, 1.4, 100, 100);
 
-            glPushMatrix();
-                glTranslatef(0,0,1.4);
-                glColor3f(0,0,1);
-                glutSolidSphere(0.3,100,100); // pe
+                    glPushMatrix();
+                        glTranslatef(0,0,1.4);
+                        glColor3f(0,0,1);
+                        glutSolidSphere(0.3,100,100); // pe
 
-                glColor3f(1,1,1);
+                        glColor3f(0.5,0.5,0.5);
 
-                glRotatef(90,1,0,0);
-                glTranslatef(0,0.25,0);
-                glScalef(1,0.2,10);
-                glutSolidSphere(0.35,100,100); // esqui
-
-            glPopMatrix();
+                        glRotatef(90,1,0,0);
+                        glTranslatef(0,0.25,0);
+                        glScalef(1,0.2,10);
+                        glutSolidSphere(0.35,100,100); // esqui
+                    glPopMatrix();
+                glPopMatrix();
+        glPopMatrix();
         glPopMatrix();
 
-        glPushMatrix(); //perna esquerda
-            glColor3f(1.0, 0.85, 0.75);
-            glRotatef(90,1,0,0);
-            glTranslatef(-0.4,0,1.9);
-            gluDisk(obj,0,raioMembros,100,100);
-            gluCylinder(obj, raioMembros, raioMembros, 1.5, 100, 100);
-
-            glTranslatef(0,0,1.5);
-            glutSolidSphere(0.22,100,100); ///joelho
-
-            gluDisk(obj,0,raioMembros,100,100);
-            gluCylinder(obj, raioMembros, raioMembros, 1.4, 100, 100);
-
-            glPushMatrix();
-                glTranslatef(0,0,1.4);
-                glColor3f(0,0,1);
-                glutSolidSphere(0.3,100,100); /// pe
-
-                glColor3f(1,1,1);
-
+        glPushMatrix();
+            glColor3f(1,1,1);
+            glTranslatef(-0.4,-1.9,0);
+            glutSolidSphere(0.22,100,100); /// ///bumbum tam tam
+            glPushMatrix(); //perna esquerda
+                glColor3f(1.0, 0.85, 0.75);
                 glRotatef(90,1,0,0);
-                glTranslatef(0,0.25,0);
-                glScalef(1,0.2,10);
-                glutSolidSphere(0.35,100,100); /// esqui
+                gluDisk(obj,0,raioMembros,100,100);
+                gluCylinder(obj, raioMembros, raioMembros, 1.5, 100, 100);
+
+                glTranslatef(0,0,1.5);
+                glutSolidSphere(0.22,100,100); ///joelho
+
+                gluDisk(obj,0,raioMembros,100,100);
+                gluCylinder(obj, raioMembros, raioMembros, 1.4, 100, 100);
+
+                glPushMatrix();
+                    glTranslatef(0,0,1.4);
+                    glColor3f(0,0,1);
+                    glutSolidSphere(0.3,100,100); /// pe
+
+                    glColor3f(0.5,0.5,0.5);
+
+                    glRotatef(90,1,0,0);
+                    glTranslatef(0,0.25,0);
+                    glScalef(1,0.2,10);
+                    glutSolidSphere(0.35,100,100); /// esqui
+                glPopMatrix();
             glPopMatrix();
         glPopMatrix();
 
     glPopMatrix();
 
-    glFlush();
+    glutSwapBuffers();
 }
 
 void transformacoes(int key, int x, int y){
@@ -180,11 +208,70 @@ void transformacoes(int key, int x, int y){
            angY+=15;
            break ;
         default:
-           break;
+           	break ;
      }
      glutPostRedisplay() ;
 }
 
+
+void agaixar(int value)
+{
+	int i, aux = 0;
+	if(aux == 0)
+    	perna-=2;
+	if(perna == 60)
+		aux = 1;
+
+	// Redesenha a cena com as novas coordenadas
+	glutPostRedisplay();
+
+	if(aux == 0)
+		glutTimerFunc(10,agaixar, 1);
+}
+
+void movimentos(unsigned char key, int x, int y){
+	switch (key){
+		case 'a' :
+		case 'A' :
+			trx-=1;
+			try-=1;
+			break;
+		case 'D' :
+		case 'd' :
+			trx+=1;
+			try+=1;
+			break ;
+        case 's' :
+        case 'S' :
+            glutTimerFunc(10,agaixar,1);
+ 		default:
+           	break ;
+	}
+	glutPostRedisplay() ;
+}
+
+void timerFunc(int value)
+{
+
+    if(flag == 0){
+        ombro+=3;
+        antebraco+=2.5;
+    }
+    else if (flag == 1){
+        ombro-=3;
+        antebraco-=2.5;
+    }
+
+    if (ombro == 45)
+        flag = 1;
+    else if(ombro == -45)
+
+        flag = 0;
+
+	// Redesenha a cena com as novas coordenadas
+	glutPostRedisplay();
+	glutTimerFunc(10,timerFunc, 1);
+}
 
 int main(int argc, char *argv[])
 {
@@ -195,6 +282,9 @@ int main(int argc, char *argv[])
      glutCreateWindow("Primeiro esboço - Personagem Esqui nos Alpes");
      glutDisplayFunc(display);
      glutSpecialFunc(transformacoes);
+	 glutKeyboardFunc(movimentos);
+     glutTimerFunc(10,timerFunc,1);
      init();
      glutMainLoop();
 }
+
